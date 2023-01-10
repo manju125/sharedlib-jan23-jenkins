a45e34561f432aa02e504293eebe67d5a2ec5904
innovative <innovative@gmail.com>
2023-01-10 13:09:53 +0000

    shared library

---
+++ src/com/jenkinslib/GlobalVars.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+package com.jenkinslib
+
+class GlobalVars {
+  static String foo = "bar"
+
+  // In Pipeline static variable can be used：
+  // 
+  // import com.jenkinslib.GlobalVars
+  // println GlobalVars.foo
+}
